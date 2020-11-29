f35fb5838b73988c75fed500d47e6d5eb3866ceb
fangbinwei <fangbinwei@yeah.net>
2020-11-29 21:39:53 +0800

    ci: patch chromedriver

--- scripts/patchChromedriver.js
+++ scripts/patchChromedriver.js
@@ -5,8 +5,8 @@ const fs = require('fs')
 const path = require('path')
 const pkg = require('../package.json')
 
-const versionString = require('child_process').execSync('wmic datafile where name="C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value').toString()
-const majorVersion = versionString.match(/Version=(\d+)/)[1]
+// const versionString = require('child_process').execSync('wmic datafile where name="C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value').toString()
+const majorVersion = '86'
 pkg.resolutions.chromedriver = `^${majorVersion}.0.0`
 
 console.log(`patched chromedriver to version ${majorVersion}`)
